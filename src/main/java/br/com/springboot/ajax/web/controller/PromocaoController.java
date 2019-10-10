2448c86455e328e979af21069f3172a52356fd41
delfinopaulaemerson <delfs@hotmail.com>
2019-10-10 00:06:50 -0300

    AutoComplete com JqueryUi e filro por nome

--- src/main/java/br/com/springboot/ajax/web/controller/PromocaoController.java
+++ src/main/java/br/com/springboot/ajax/web/controller/PromocaoController.java
@@ -10,14 +10,20 @@ import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import br.com.springboot.ajax.web.domain.Categoria;
 import br.com.springboot.ajax.web.domain.Promocao;
@@ -27,45 +33,95 @@ import br.com.springboot.ajax.web.repository.PromocaoRepository;
 @Controller
 @RequestMapping("/promocao")
 public class PromocaoController {
-	
+
 	private static Logger LOG = LoggerFactory.getLogger(PromocaoController.class);
-	
+
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	@Autowired
-	private PromocaoRepository PromocaoRepository; 
-	
+	private PromocaoRepository PromocaoRepository;
+
 	@GetMapping("/add")
 	public String abrirCadastro() {
-		
+
 		return "promo-add";
 	}
-	
+
 	@ModelAttribute("categorias")
-	public List<Categoria> getcategorias(){
-		
+	public List<Categoria> getcategorias() {
+
 		return this.categoriaRepository.findAll();
 	}
-	
+
 	@PostMapping("/save")
-	public ResponseEntity<?>salvarPromocao(@Valid Promocao promocao, BindingResult result) {
-		
-		if(result.hasErrors()) {
-			
+	public ResponseEntity<?> salvarPromocao(@Valid Promocao promocao, BindingResult result) {
+
+		if (result.hasErrors()) {
+
 			Map<String, String> erros = new HashMap<>();
-			for(FieldError error: result.getFieldErrors()) {
+			for (FieldError error : result.getFieldErrors()) {
 				erros.put(error.getField(), error.getDefaultMessage());
 			}
-			
+
 			return ResponseEntity.unprocessableEntity().body(erros);
 		}
-		
+
 		promocao.setDtCadastro(LocalDateTime.now());
 		this.PromocaoRepository.save(promocao);
+
+		return ResponseEntity.ok().build();
+
+	}
+
+	@GetMapping("/list")
+	public String listaPromocoes(ModelMap map) {
+		Sort sort = new Sort(Sort.Direction.DESC, "dtCadastro");
+		PageRequest pageRequest = PageRequest.of(0, 8, sort);
+		map.addAttribute("promocoes", this.PromocaoRepository.findAll(pageRequest));
+
+		return "promo-list";
+	}
+	
+	@GetMapping("/list/ajax")
+	public String listaCards(@RequestParam(name = "page", defaultValue = "1") int page,
+							 @RequestParam(name = "site", defaultValue = "") String site, ModelMap model) {
 		
-		return ResponseEntity.ok().build(); 
+		Sort sort = new Sort(Sort.Direction.DESC, "dtCadastro");
+		PageRequest pageRequest = PageRequest.of(page, 8, sort);
+		if(site.isEmpty()) {
+			model.addAttribute("promocoes", this.PromocaoRepository.findAll(pageRequest));
+		}else {
+			model.addAttribute("promocoes", this.PromocaoRepository.findBySite(site, pageRequest));
+		}
+
+		return "promo-card";
+	}
+	
+	@PostMapping("/like/{id}")
+	public ResponseEntity<?> adicionarLikes(@PathVariable("id") Long id) {
+		int likes = 0;
+		this.PromocaoRepository.updateSomarLikes(id);
+		likes = this.PromocaoRepository.findLikesById(id);
+				
+		return ResponseEntity.ok(likes);
+	}
+	
+	
+	@GetMapping("/site")
+	public ResponseEntity<?> findSiteByTermo(@RequestParam("termo") String termo){
+		List<String> sites = this.PromocaoRepository.findSiteByTermo(termo);
 		
+		return ResponseEntity.ok(sites);
+	}
+	
+	@GetMapping("/site/list")
+	public String findBySite(@RequestParam("site") String site, ModelMap model){
+		Sort sort = new Sort(Sort.Direction.DESC, "dtCadastro");
+		PageRequest pageRequest = PageRequest.of(0, 8, sort);
+		model.addAttribute("promocoes", this.PromocaoRepository.findBySite(site,pageRequest));
+		
+		return "promo-card";
 	}
 
 }
--- src/main/java/br/com/springboot/ajax/web/repository/PromocaoRepository.java
+++ src/main/java/br/com/springboot/ajax/web/repository/PromocaoRepository.java
@@ -1,9 +1,32 @@
 package br.com.springboot.ajax.web.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.springboot.ajax.web.domain.Promocao;
 
 public interface PromocaoRepository extends JpaRepository<Promocao, Long>{
+	
+	@Transactional(readOnly = false)
+	@Modifying
+	@Query("update Promocao p set p.likes = p.likes + 1 where p.id= :id")
+	void updateSomarLikes(@Param("id")Long id);
+	
+	@Query("select p.likes from Promocao p where p.id = :id")
+	int findLikesById(@Param("id") Long id);
+	
+	
+	@Query("select distinct p.site from Promocao p where p.site like %:site%")
+	List<String> findSiteByTermo(@Param("site") String site);
+	
+	@Query("select p from Promocao p where p.site like :site")
+	Page<Promocao> findBySite(@Param("site") String site, Pageable pageable);
 
 }
